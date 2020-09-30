c329b4487239aefb1d282b3c7981807d699ce11b
Koi <Makoi.Gacula@student.csulb.edu>
2020-09-29 20:03:43 -0700

    Created an event handler to change contents of header

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-//DOM Manipulation
-/** document is the DOM object we want to access and this is an example how we change HTML
+//DOM Manipulation (Dynamic HTML)
+/** document is the document object that represents our webpage
+ * This is an example how we change the inner HTML of an element
  *
- * getElementById() is a method we used to access the id of an element
+ * getElementById(id) is a method we used to get the id of an element
+ * getElementsByClassName(classnName) a method to find element by class name
  * innerHTML is a property, used to get or change the content of HTML elements
  */
 document.getElementById("header-title").innerHTML = "Hi!";
+
+// Changing the value of an attribute for an HTML element:
+// element.setAttribute(attribute, value)
+
+// Adding Event Handlers
+document.getElementById("header2").onclick = function () {
+  //changing the contents of the header upon clicking
+  document.getElementById("header2").innerHTML = "Clicked!";
+};
+
+// We can store HTML elements into variables, returned as objects
+var myElement = document.getElementById("header3");
+document.getElementById("header3").innerHTML =
+  "The old content in this div block was:" + myElement.innerHTML;
+
+//We can also get elements by tag name (gets a paragraph tag)
+//Ex) document.getElementbyTagName("p");
