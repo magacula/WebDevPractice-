170fba3e29971851f32190059d885286a49b3800
Koi <Makoi.Gacula@student.csulb.edu>
2020-09-29 19:34:58 -0700

    created script.js and adding DOM manipulation to header

---
+++ script.js
@@ -0,0 +1,7 @@
+//DOM Manipulation
+/** document is the DOM object we want to access and this is an example how we change HTML
+ *
+ * getElementById() is a method we used to access the id of an element
+ * innerHTML is a property, used to get or change the content of HTML elements
+ */
+document.getElementById("header-title").innerHTML = "Hi!";
